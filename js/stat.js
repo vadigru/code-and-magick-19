9d928f82cd9806aa3936cc5ae04456f7ffcae403
Vadim Grunenko <vg@outlook.ie>
2020-01-23 13:21:18 +0200

    Adds changes according mentor comments

--- js/stat.js
+++ js/stat.js
@@ -26,7 +26,7 @@ var renderText = function (ctx, text, x, y, color, fontFace) {
 };
 
 var getMaxNumber = function (array) {
-  var max = -Infinity;
+  var max = array[0];
   array.forEach(function (item) {
     if (item > max) {
       max = item;
@@ -43,14 +43,18 @@ var getRandomSaturation = function () {
   return Math.floor(Math.random() * 100);
 };
 
+var selectColor = function (el) {
+  return (el === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'hsl(245,' + getRandomSaturation() + '%, 50%)';
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  renderRect(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.3)');
+  renderRect(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
   renderRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(255, 255, 255, 1)');
   renderText(ctx, 'Ура вы победили!', CLOUD_TEXT_X, CLOUD_TEXT_Y);
   renderText(ctx, 'Список результатов:', CLOUD_TEXT_X, CLOUD_TEXT_Y + GAP + GAP);
   names.forEach(function (item, i) {
+    var barColor = selectColor(item);
     var barHeight = getBarHeight(times[i], times);
-    var barColor = (item === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'hsl(245,' + getRandomSaturation() + '%, 50%)';
     renderRect(ctx, BAR_X + BAR_WIDTH * i + BAR_SPACE * i, BAR_Y + BAR_DEFAULT_HEIGHT - barHeight - GAP, BAR_WIDTH, barHeight, barColor);
     renderText(ctx, Math.round(times[i]), BAR_X + BAR_WIDTH * i + BAR_SPACE * i, BAR_TEXT_TOP + BAR_DEFAULT_HEIGHT - barHeight - GAP);
     renderText(ctx, item, BAR_X + BAR_WIDTH * i + BAR_SPACE * i, BAR_TEXT_BOTTOM);
